83301fa078d25724b8c0744d28efa54ac9ab3c1c
Kirill <91422323+k1dobu3@users.noreply.github.com>
2021-10-31 18:15:34 +0300

    Add files via upload

---
+++ DniNedeli/DniNedeli/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DniNedeli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер дня недели:");
+            double x = Convert.ToInt32(Console.ReadLine());
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("Понедельник");
+                    break;
+                case 2:
+                    Console.WriteLine("Вторник");
+                    break;
+                case 3:
+                    Console.WriteLine("Среда");
+                    break;
+                case 4:
+                    Console.WriteLine("Четверг");
+                    break;
+                case 5:
+                    Console.WriteLine("Пятница");
+                    break;
+                case 6:
+                    Console.WriteLine("Суббота");
+                    break;
+                case 7:
+                    Console.WriteLine("Воскресенье");
+                    break;
+                default:
+                    Console.WriteLine("Net sootvetstviya!");
+                    break;
+            }
+        }
+    }
+}
